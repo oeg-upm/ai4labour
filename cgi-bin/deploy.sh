47eb6b1a4525838da619266be56e4b8b52f6df0f
Victor Rodriguez Doncel <vrodriguez@fi.upm.es>
2023-08-03 13:48:38 +0200

    adsf

--- cgi-bin/deploy.sh
+++ cgi-bin/deploy.sh
@@ -2,10 +2,6 @@
 echo "Content-type: text/html"
 echo
 echo "<html><body>wait 5 seconds</body></html>"
-sudo systemctl stop apache2
 cd /var/www/html
-sudo rm -rf *
-sudo rm -rf .git
-sudo git clone "https://${GITHUB_TOKEN}@github.com/oeg-upm/ai4labour.git" .
-sudo systemctl start apache2
-echo "<html><body>wait x seconds</body></html>"
+git pull
+
