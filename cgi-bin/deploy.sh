8eda2ad09ceb34f71f33416afb9cc1ec847fb7fc
Victor Rodriguez Doncel <vrodriguez@fi.upm.es>
2023-08-03 13:05:05 +0200

    asdf

--- cgi-bin/deploy.sh
+++ cgi-bin/deploy.sh
@@ -1,8 +1,7 @@
-#!/bin/bash
+!/bin/bash
 sudo systemctl stop apache2
 cd /var/www/html
 sudo rm -rf *
 sudo rm -rf .git
-direccion=http://"$GITHUB_TOKEN"@github.com/oeg-upm/ai4labour.git
-sudo git clone $direccion .
+sudo git clone "https://${GITHUB_TOKEN}@github.com/oeg-upm/ai4labour.git" .
 sudo systemctl start apache2
