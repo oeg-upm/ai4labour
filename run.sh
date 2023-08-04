4612b5863549fbcc936b47b7ab01e31f29b2a2e7
vroddon <vrodriguez@fi.upm.es>
2023-08-04 07:04:04 +0000

    permisos

--- run.sh
+++ run.sh
@@ -1,6 +1,3 @@
 #!/bin/sh
-#Los primeros con background, pero el ultimo no debe morir
-#sudo python3 /nlp/wordfreq/wf.py&
-#sudo python3 /nlp/semanticfields/sf.py&
-sudo /usr/bin/java -jar /victor/target/crec-1.0.jar --server.port=8080 --server.use-forward-headers=true
+sudo /usr/bin/java -jar /victor/ai4labour/target/crec-1.0.jar --server.port=8080 --server.use-forward-headers=true
 
